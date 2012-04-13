7aaa028c20ab67124d64a7cd200bdee1338c840f
David Pollak <feeder.of.the.bears@gmail.com>
2012-04-13 10:35:02 -0700

    Correct calculation of the value of a menu item

--- src/main/scala/code/snippet/CurrentSearch.scala
+++ src/main/scala/code/snippet/CurrentSearch.scala
@@ -2,6 +2,7 @@ package code.snippet
 
 
 import net.liftweb._
+import common._
 import util._
 import Helpers._
 import net.liftweb.http.TransientRequestVar
@@ -11,12 +12,16 @@ import code.model.Search
 import code.lib.Routing
 
 
+object Results {
+  def render = "*" #> CurrentSearch.get.map(_.toString)
+}
+
 trait CurrentSearch
 
-object CurrentSearch extends TransientRequestVar[Box[Search]](None) with CurrentSearch {
+object CurrentSearch extends TransientRequestVar[Box[Search]](Empty) with CurrentSearch {
 
-  def apply() = get
-  def apply(maybeId:String) = setIsUnset(Option(Search(UUID.fromString(maybeId))))
+  def apply(): Box[Search] = get
+  def apply(maybeId:String): Box[Search] = setIsUnset(Full(Search(UUID.fromString(maybeId))))
 
   /**
    * Gets the current search id
